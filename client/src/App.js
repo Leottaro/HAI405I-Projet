f515b94ca618755d17a1205598c5aab694aed903
Leottaro <leo.hafdane@gmail.com>
2023-12-12 18:04:23 +0100

    feature plusieurs pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import { useEffect, useState } from "react";
-import './App.css';
+import { useNavigate, Routes, Route } from "react-router-dom";
 import socket from "./socket";
+import './App.css';
 
-import Connection from "./connection/Connection";
+import Connection from "./pages/connection/Connection";
+import SelectionJeux from "./pages/selectionJeux/selectionJeux";
 
 function App() {
+  const navigate = useNavigate();
+
+  socket.on("goTo", page => {
+    navigate(page);
+  });
+
   return (
-    <Connection/>
+    <Routes>
+      <Route exact path="/" element={<Connection />} />
+      <Route path="/Connection" element={<Connection />} />
+      <Route path="/selectionJeux" element={<SelectionJeux />} />
+    </Routes>
   );
 }
-
 export default App;
