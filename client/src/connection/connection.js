badb3375767ec1d337e9dda293b720be48dc1d59
Leottaro <leo.hafdane@gmail.com>
2023-12-12 15:21:27 +0100

    added connection and account system

--- client/src/connection/connection.js
+++ client/src/connection/connection.js
@@ -1,3 +1,4 @@
+import "./connection.css";
 import { useEffect, useState } from "react";
 import socket from "../socket";
 
@@ -5,15 +6,20 @@ function Connection() {
     const [pseudo, setPseudo] = useState("");
     const [password, setPassword] = useState("");
 
-    function valider() {
-        socket.emit("reqRegister", [pseudo, password]);
+    function register() {
+        socket.emit("reqRegister", { "pseudo": pseudo, "password": password });
+    }
+
+    function connect() {
+        socket.emit("reqConnect", { "pseudo": pseudo, "password": password });
     }
 
     return (
-        <div id="connection">
+        <div id="connectionDiv">
             <input placeholder="Entrez un nom" type="text" onChange={(event) => { setPseudo(event.target.value) }}></input>
             <input placeholder="Entrez le mot de passe" type="password" onChange={(event) => { setPassword(event.target.value) }}></input>
-            <button onClick={valider}>Valider</button>
+            <button onClick={register}>S'inscrire</button>
+            <button onClick={connect}>Se connecter</button>
         </div>
     );
 }
