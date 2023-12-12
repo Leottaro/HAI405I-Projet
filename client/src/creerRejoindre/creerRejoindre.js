20defdd2a73ac8e6285f5d6017c5fa895cb3f633
Leottaro <leo.hafdane@gmail.com>
2023-12-12 16:07:27 +0100

    modified App.css

--- client/src/creerRejoindre/creerRejoindre.js
+++ client/src/creerRejoindre/creerRejoindre.js
@@ -2,3 +2,14 @@ import { useEffect, useState } from "react";
 import socket from "../socket";
 import Creer from "./creer";
 import Rejoindre from "./rejoindre";
+import "./creerRejoindre.css";
+
+function CreerRejoindre(){
+
+    return (
+    <div class = 'creerRejoindre'>
+        <Creer/>
+    </div>
+);}
+export default CreerRejoindre;
+
