f33452790d69ddcf9334d77ee980e5b924965ae4
chrisRoman34 <chris34730@gmail.com>
2023-12-14 15:33:30 +0100

    modif creerRejoindre

--- client/src/creerRejoindre/creer.js
+++ client/src/creerRejoindre/creer.js
@@ -4,18 +4,20 @@ import "./creer.css";
 import { useNavigate } from "react-router-dom";
 
 function Creer() {
-    let lien="";
     function creerLien() {
+        let newLien=0;
         for(let i=0;i<8;i++){
-            lien=10*lien+Math.floor(Math.random()*9);
+            newLien=10*newLien+Math.floor(Math.random()*9);
         }
+        setLien(newLien);
         console.log(lien);
         valider()
     }
     const [nbrJoueurs, setNbrJoueurs] = useState(1);
+    const [lien, setLien] = useState(0);
 
     function valider() {
-        socket.emit("reqLienPartie", [nbrJoueurs, lien]);
+        socket.emit("reqCreate", {nbrJoueurs: nbrJoueurs, lien: lien, jeux: "bataille"});
     }
 
     const navigate=useNavigate();
@@ -26,10 +28,13 @@ function Creer() {
 
     return (
         <div id="creerDiv">
-            <button className='rejoindre' onClick={rejoindre}>Rejoindre</button>
-            <button className="rejoindreButton">Creer</button>
-            <label>entrez le nombre de joueurs dans la partie:</label>
-            <input defaultValue={1} type="number" min={1} max={6} onChange={(event) => 
+            <div id="creerRejoindre">
+                <button id="creerButton">Creer</button>
+                <button id="rejoindreButton" onClick={rejoindre}>Rejoindre</button>
+            </div>
+            
+            <label>Nombre de joueurs dans la partie:</label>
+            <input className="nbrJoueursInput" defaultValue={2} type="number" min={2} max={10} onChange={(event) => 
                 {setNbrJoueurs(parseInt(event.target.value)) }}></input>
             <button className='validerButton' onClick={creerLien}>Valider</button>
             <label>copiez le lien de la partie:</label>
--- client/src/creerRejoindre/rejoindre.js
+++ client/src/creerRejoindre/rejoindre.js
@@ -7,21 +7,28 @@ function Rejoindre() {
 
     const [lien, setLienPartie] = useState(1);
     const navigate=useNavigate();
-    
+    const [message, setMessage] = useState("");
+
     function creer() {
         navigate("/creer");
     }
 
     function valider(){
-        socket.emit("reqDemandeEntrePartieLien", [lien]);
+        socket.emit("reqJoin", lien);
     }
+    socket.on('resJoin',json => {
+        setMessage(json.message)
+    });
     return (
         <div id="rejoindreDiv">
-            <button className="creerButton"onClick={creer}>Creer</button>
-            <button className="rejoindreButton">Creer</button>
+            <div id="creerRejoindre">
+                <button id="creerButton"onClick={creer}>Creer</button>
+                <button id="rejoindreButton">Rejoindre</button>
+            </div>
             <label>entrez un lien de partie</label>
-            <input type="number" max={9999999} onChange={(event) => {setLienPartie(parseInt(event.target.value)) }}></input>
+            <input className="lienInput" type="number" max={9999999} onChange={(event) => {setLienPartie(parseInt(event.target.value)) }}></input>
             <button className="submitButton" onClick={valider}>submit</button>
+            <label id="message">{message}</label>
             <label>Parties en cours:</label>
         </div>
     );
