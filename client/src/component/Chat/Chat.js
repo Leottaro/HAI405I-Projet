fdfb11c6f9614f7cc562ebf96b881b486ab760da
Warglabidoo <153500769+Warglabidoo@users.noreply.github.com>
2023-12-15 14:51:11 +0100

    le chat marche

--- client/src/component/Chat/Chat.js
+++ client/src/component/Chat/Chat.js
@@ -1,45 +1,45 @@
 import { useEffect, useState } from "react";
-import socket from "../../socket";
+import socket, {account} from "../../socket";
 import './Chat.css';
 
 function Chat(){
     const [listeMsg, setListeMsg] = useState([]);
-    
-    useEffect(()=>{
-        
-        let interval = setInterval(afficherChat,1000);
-    }, []);
+    const [open, setOpen] = useState(true);
 
-    socket.on("nouveauMsg", msg => {
+    socket.on("resMsg", msg => {
         let temp=listeMsg;
-        temp.concat(msg);
+        temp=temp.concat(msg);
+        
         setListeMsg(temp);
+        console.log("msg : ",msg)
+        console.log("temp : ",temp)
     })
 
-    function afficherChat(){
-        setListeMsg([{pseudo: "moi", msg: "bonjour je suis moi meme et je mange des artichauds et des bananes"}, {pseudo: "pasmoi", msg: "salut"},{pseudo: "moi", msg: "bonjour je suis moi meme et je mange des artichauds et des bananes"},{pseudo: "moi", msg: "bonjour je suis moi meme et je mange des artichauds et des bananes"},{pseudo: "moi", msg: "bonjour je suis moi meme et je mange des artichauds et des bananes"},{pseudo: "moi", msg: "bonjour je suis moi meme et je mange des artichauds et des bananes"},{pseudo: "moi", msg: "bonjour je suis moi meme et je mange des artichauds et des bananes"},{pseudo: "moi", msg: "bonjour je suis moi meme et je mange des artichauds et des bananes"},{pseudo: "moi", msg: "bonjour je suis moi meme et je mange des artichauds et des bananes"},{pseudo: "moi", msg: "bonjour je suis moi meme et je mange des artichauds et des bananes"}]);
+    function envoyer(){
+        socket.emit("reqMsg",[{pseudo: account, msg: document.getElementById("inputMsg").value}]);
     }
 
-    function envoyer(){
-        afficherChat();
-        console.log(listeMsg);
-        socket.emit("msg",document.getElementById("inputMsg").value);
+
+    function maskChat(){
+        setOpen(!open);
+
     }
 
     return (
-        <div>
+        <div className={open ? "" : "closed"}>
             <div className="chat">
                 {listeMsg.map((msg, index) => (
-                    <div className="auteurMsg">
+                    <div className="auteurMsg" key={index}>
                         <label className="auteur">{msg.pseudo} :</label>
                         <label className="msg">{msg.msg}</label>
                     </div>
                 ))}
-                
+            
             
             </div>
             <input id="inputMsg" type="text"></input>
             <button id="envoyerMsg" onClick={envoyer}>Envoyer</button>
+            <button id="maskChat"  onClick={maskChat}>{open ? ">" : "<"}</button>
         </div>
     )
 }
