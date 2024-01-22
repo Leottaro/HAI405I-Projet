ec696f8b559f7259e28babcc74c5b0284bfe56b7
Mxerox <charlyga04@gmail.com>
2024-01-22 14:07:52 +0100

    set de la page du scoreboard

--- client/src/pages/score/score.js
+++ client/src/pages/score/score.js
@@ -5,13 +5,38 @@ import { useNavigate, useParams } from "react-router-dom";
 
 function Score() {
     const [gagnant, setGagnant] = useState();
-
+    const [listeJoueurs, setListeJoueurs] = useState([]);
+    const [score, setScore] = useState({ nom: "", score : });
     socket.on("resGagnant", nom => {
         setGagnant(nom);
     });
+
+    socket.on("listJoueur", liJoueurs => {
+        setListeJoueurs(liJoueurs);
+    });
+    socket.on("scorePartie", dicoScore => {
+        setScore(dicoScore);
+    });
+
     return (
         <div id="scoreDiv">
             <label id="messageFin">{gagnant} a gagner la partie!</label>
+            <div id="scoreBoard">
+                <table>
+                <thead>
+                    <tr>
+                        <th colspan="2">Tableau des scores</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {listeJoueurs.map((nom) => {
+                <tr>
+                    <td>{nom}</td>
+                    <td>{dicoScore[nom]}</td>
+                </tr>})}
+                </tbody>
+                </table>
+            </div>
         </div>
     );
 }
