866d7c79f6236aa8fa06fcbfbb81eab6d7866429
Leottaro <leo.hafdane@gmail.com>
2024-01-22 14:12:32 +0100

    cartes Six bien distribué

--- server/games/SixQuiPrend.js
+++ server/games/SixQuiPrend.js
@@ -1,3 +1,5 @@
+const Carte = require("./Carte");
+
 class SixQuiPrend {
     static playersRange = [2, 10];
 
@@ -57,17 +59,16 @@ class SixQuiPrend {
         // créer le paquet de carte et le mélanger
         const Cartes = [...Array(104).keys()].map(n => { return { valeur: n + 1, type: "" } });
         for (let i = 0; i < Cartes.length; i++) {
-            for (let j = i; j < Cartes.length; j++) {
-                [Cartes[i], Cartes[j]] = [Cartes[j], Cartes[i]];
-            }
+            const j = Math.floor(i + Math.random() * (Cartes.length - i));
+            [Cartes[i], Cartes[j]] = [Cartes[j], Cartes[i]];
         }
         // distribuer 10 cartes
         let n = 0;
         for (const playerID of this.playersIDs) {
             for (let i = 0; i < 10; i++) {
                 this.paquets[playerID].push(Cartes[n]);
+                n++;
             }
-            n++;
         }
         for (const paquet of this.plateau) {
             paquet.push(Cartes[n]);
@@ -99,32 +100,8 @@ class SixQuiPrend {
         if (this.ended || !this.everyonePlayed()) {
             return false;
         }
-        let winner;
-        let sortedChoosed = this.enLice.sort((id1, id2) => Carte.sort(this.choosed[id1], this.choosed[id2], true));
-        let nbEgalite = 1;
-        for (let i = 1; i < sortedChoosed.length; i++) {
-            if (this.choosed[sortedChoosed[i]].valeur == this.choosed[sortedChoosed[0]].valeur) {
-                this.tempCartes.push(this.choosed[sortedChoosed[i]]);
-                delete this.choosed[sortedChoosed[i]];
-                nbEgalite = i + 1;
-            }
-        }
-        if (nbEgalite == 1) {
-            winner = sortedChoosed[0];
-        }
-        else {
-            this.tempCartes.push(this.choosed[sortedChoosed[0]]);
-            delete this.choosed[sortedChoosed[0]];
-            this.enLice = sortedChoosed.slice(0, nbEgalite);
-            return this.nextRound();
-        }
-        this.paquets[winner] = this.paquets[winner].concat(Object.values(this.choosed).concat(this.tempCartes));
-        this.tempCartes = [];
-        this.choosed = {};
-        this.round++;
-        this.enLice = this.playersIDs.filter(playerID => this.paquets[playerID].length > 0);
-        this.ended = this.enLice.length <= 1;
-        return true;
+        const choosed = Object.values(this.choosed).sort((a, b) => a.valeur - b.valeur);
+        console.log(choosed);
     }
 }
 module.exports = SixQuiPrend;
