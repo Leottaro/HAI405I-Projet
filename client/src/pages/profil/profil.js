0a4507a9f39c6883954f8c25a89654e1f804e43a
Warglabidoo <153500769+Warglabidoo@users.noreply.github.com>
2024-01-24 16:31:02 +0100

    profil prends en compte la database

--- client/src/pages/profil/profil.js
+++ client/src/pages/profil/profil.js
@@ -3,7 +3,55 @@ import "./profil.css";
 import { useEffect, useState } from "react";
 
 function Profil(){
-    const [historique, setHistorique] = useState([]);
+    const [parties, setParties] = useState([]);
+    const [nbSix, setNbSix] = useState(0);
+    const [nbBataille, setNbBataille] = useState(0);
+    const [winSix, setWinSix] = useState(0);
+    const [winBataille, setWinBataille] = useState(0);
+    const [winrateSix, setWinrateSix] = useState(0);
+    const [winrateBataille, setWinrateBataille] = useState(0);
+    useEffect(() => {
+        socket.emit("reqProfilStat");
+    }, []);
+
+    socket.on("resProfilStat", data => {
+        setParties(data);
+        let tempNbSix=0;
+        let tempNbBataille=0;
+        let tempWinSix=0;
+        let tempWinBataille=0;
+        let tempPlaceSix=0;
+        let tempPlaceBataille=0;
+        parties.forEach(partie => {
+            switch (partie.nomJeux){
+                case "sixQuiPrend":
+                    console.log("six qui prends");
+                    tempNbSix+=1;
+                    if(partie.place==1){
+                        tempWinSix+=1;
+                    }
+                    tempPlaceSix+=partie.place;
+                    break;
+                case "bataille":
+                    console.log("bataille");
+                    tempNbBataille+=1;
+                    if(partie.place==1){
+                        tempWinBataille+=1;
+                    }
+                    tempPlaceBataille+=partie.place;
+                    break;
+                default:
+                    break;
+            }
+        })
+        setNbSix(tempNbSix);
+        setNbBataille(tempNbBataille);
+        setWinSix(tempWinSix);
+        setWinBataille(tempWinBataille);
+        setWinrateSix(tempPlaceSix/tempNbSix);
+        setWinrateBataille(tempPlaceBataille/tempNbBataille);
+    });
+
 
     return (
         <div id="profil">
@@ -12,28 +60,28 @@ function Profil(){
                 <div id="profilJeux">
                     <div className="profilJeux">
                         <div className="stat">
-                            <label id="nomBataille">Bataille</label>
-                            <label className="labelProfil">Parties jouées : </label>
-                            <label className="labelProfil">Victoires : </label>
-                            <label className="labelProfil">Winrate : </label>
+                            <label className="nomJeux">Bataille</label>
+                            <label className="labelProfil">Parties jouées : {nbBataille}</label>
+                            <label className="labelProfil">Victoires : {winBataille}</label>
+                            <label className="labelProfil">Placement Moyen : {winrateBataille}</label>
                         </div>
                     </div>
                     <div className="profilJeux">
                         <div className="stat">
-                            <label id="nomSix">Six Qui Prend</label>
-                            <label className="labelProfil">Parties jouées : </label>
-                            <label className="labelProfil">Victoires : </label>
-                            <label className="labelProfil">Placement moyen : </label>
+                            <label className="nomJeux">Six Qui Prend</label>
+                            <label className="labelProfil">Parties jouées : {nbSix}</label>
+                            <label className="labelProfil">Victoires : {winSix}</label>
+                            <label className="labelProfil">Placement Moyen : {winrateSix}</label>
                         </div>
                     </div>
                 </div>
                 <div id="historique">
                     <label id="labelHistorique">Historique</label>
                     <div id="scrollHistorique">
-                        {historique.map(json => 
-                            <div className="partie">
-                                <label className="mode">{json.mode}</label>
-                                <label className="resultat">{json.resultat}</label>
+                        {parties.map(json => 
+                            <div className={json.place==1 ? "victoire" : "defaite"}>
+                                <label className="mode">{json.nomJeux}</label>
+                                <label className="resultat">{json.place}</label>
                             </div>
                         )}
                     </div>
