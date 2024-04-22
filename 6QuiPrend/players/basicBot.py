8bff597f0037d880cb6dc0c469ea134b8a84a152
Warglabidoo <153500769+Warglabidoo@users.noreply.github.com>
2024-04-22 16:03:25 +0200

    bot qui joue le meilleur coup sans regarder(nul)

---
+++ 6QuiPrend/players/basicBot.py
@@ -0,0 +1,32 @@
+from random import randint
+from players.bot import Bot
+from game.card import Card
+
+class BasicBot(Bot):
+    def getLineToRemove(self, game):
+        return randint(1, 4)
+
+    def getCardToPlay(self,game): 
+        
+        min=1000
+        cardMin=self.hand[0]
+        for card in self.hand:
+            placed = False
+            for i in range(len(game.table) - 1, -1, -1):
+                if game.table[i][-1]<card:
+                    if len(game.table[i]) < 5:
+                        min=0
+                        cardMin=card
+                    else:
+                        cows = game.total_cows(game.table[i])
+                        if cows<min:
+                            min=cows
+                            cardMin=card
+                    placed = True
+            if not placed:
+                for i in range(len(game.table) - 1, -1, -1):
+                    cows = game.total_cows(game.table[i])
+                    if cows < min:
+                        min = cows
+                        cardMin=card
+        return cardMin.value
