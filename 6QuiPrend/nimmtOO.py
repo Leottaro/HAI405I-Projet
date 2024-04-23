da512d70c8d6a6a1635ee1283707dd448bb92448
Leottaro <leo.hafdane@gmail.com>
2024-04-23 15:55:32 +0200

    implémentations stats 1v1

--- 6QuiPrend/nimmtOO.py
+++ 6QuiPrend/nimmtOO.py
@@ -1,10 +1,24 @@
-from players.sampleBot import SampleBot
-from players.randomBot import RandomBot
-from players.carteMinBot import CarteMinBot
+from players.basicBot import BasicBot
 from players.carteMaxBot import CarteMaxBot
+from players.carteMinBot import CarteMinBot
 from players.distanceMinBot import DistanceMinBot
+from players.minMaxBot import MinMaxBot
+from players.randomBot import RandomBot
+from players.sampleBot import SampleBot
+from players.semiRandomBot import SemiRandomBot
 from players.humanPlayer import HumanPlayer
-from game.nimmtGame import NimmtGame     
+from game.nimmtGame import NimmtGame
+
+BotsClasses = [
+    BasicBot,
+    CarteMaxBot,
+    CarteMinBot,
+    DistanceMinBot,
+    MinMaxBot,
+    RandomBot,
+    SampleBot,
+    SemiRandomBot
+]
 
 def interactiveRun():
     print("Bienvenue sur le jeu 6 qui prend !")
@@ -16,27 +30,12 @@ def interactiveRun():
             for i in range(num_players):
                 name=input("Nom du joueur : ")
                 players.append(HumanPlayer(name))
+            
+            for botClass in BotsClasses:
+                num_bot = int(input(f"Combien de {botClass.__name__} ? "))
+                for i in range(num_bot):
+                    players.append(botClass(f"{botClass.__name__}{i+1}", displayInfo=True))
 
-            num_random_bots = int(input("Combien de RandomBots ? "))
-            for i in range(num_random_bots):
-                players.append(RandomBot(f"randomBot{i+1}"))
-                
-            num_min_bots = int(input("Combien d'MinBots ? "))
-            for i in range(num_min_bots):
-                players.append(CarteMinBot(f"MinBot{i+1}"))
-                
-            num_max_bots = int(input("Combien d'MaxBots ? "))
-            for i in range(num_max_bots):
-                players.append(CarteMaxBot(f"maxBot{i+1}"))
-                
-            num_sample_bots = int(input("Combien de SampleBots ? "))
-            for i in range(num_sample_bots):
-                players.append(SampleBot(f"sampleBot{i+1}"))
-                
-            num_distanceMin_bots = int(input("Combien de DistanceMinBot ? "))
-            for i in range(num_distanceMin_bots):
-                players.append(DistanceMinBot(f"distanceMinBot{i+1}"))    
-                
             game=NimmtGame(players)
             scores, winners=game.play()
 
@@ -50,5 +49,36 @@ def interactiveRun():
         except ValueError:
             print("Veuillez entrer un nombre entier.")
 
+def statsBots():
+    bots = [botClass(botClass.__name__) for botClass in BotsClasses]
+    scoresTotaux = {bot.name: [] for bot in bots}
+    # 1v1
+    for bot1 in bots:
+        for bot2 in bots:
+            if bot1 == bot2:
+                continue
+            print(f"\n{bot1.name} vs {bot2.name}")
+            for i in range(1):
+                print(f"Partie {i+1}: ", end="")
+                game = NimmtGame([bot1, bot2])
+                scores, winners = game.play()
+                scoresTotaux[bot1.name].append(scores[bot1.name])
+                scoresTotaux[bot2.name].append(scores[bot2.name])
+                print("Gagnant(s):", ", ".join([player.name for player in winners]), "\n")
+    print("\nScores totaux:", scoresTotaux)
+
 if __name__ == "__main__":
-    interactiveRun()
+    while True:
+        try:
+            choix = int(input("\nque veux-tu faire ?\n  1: partie interactive\n  2: stats de bots\n"))
+            if choix == 1:
+                interactiveRun()
+            elif choix == 2:
+                statsBots()
+            else: 
+                Exception("Veuillez entrer 1 ou 2.")
+            break
+        except ValueError:
+            print("Veuillez entrer un nombre entier.")
+        except Exception as e:
+            print(e)
--- 6QuiPrend/players/bot.py
+++ 6QuiPrend/players/bot.py
@@ -2,8 +2,13 @@ from game.card import Card
 from players.player import Player
 
 class Bot(Player):
+    def __init__(self, name, displayInfo=False) -> None:
+        super().__init__(name)
+        self.displayInfo = displayInfo
+
     def info(self, message):
-        print("@"+self.name+" : ",message)
+        if self.displayInfo:
+            print("@"+self.name+" : ",message)
     
     def player_turn(self, game):
         return Card(self.getCardToPlay(game))
