4f39a7499d40d8f2e71ae077a0e8e853ebc96479
Matteo Planchet <matteo.planchet@gmail.com>
2024-04-30 00:51:18 +0200

    Docker images push & pull

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,5 @@
 import io from "socket.io-client";
-const host =
-    process.env.NODE_ENV === "production"
-        ? "http://jeuxapi.laboulangerie.net/"
-        : "http://localhost:3001/";
+const host = "http://localhost:3001/";
 const socket = io.connect(host);
 export default socket;
 
